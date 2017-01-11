bb44fc89cefd88ae8f7e946fe1ea3f7557761a4d
Piotr Piotrowski <piotrpiotrowski@Smarts-Mac-mini.local>
2017-01-11 13:59:16 +0100

    some code

--- DistanceConverter/ViewController.swift
+++ DistanceConverter/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let distance = Distance(miles: 1000)
 
+    @IBOutlet weak var milesTextField: UITextField!
+    @IBOutlet weak var kmTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        milesTextField.text = "\(distance.miles)"
+        kmTextField.text = "\(distance.km)"
     }
 
     override func didReceiveMemoryWarning() {
