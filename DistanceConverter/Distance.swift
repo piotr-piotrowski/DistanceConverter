52fe3915fba6f13079294eabd52b62ecbe0aef12
Piotr Piotrowski <piotrpiotrowski@Smarts-Mac-mini.local>
2017-01-11 13:53:37 +0100

    model class for Distance

---
+++ DistanceConverter/Distance.swift
@@ -0,0 +1,38 @@
+//
+//  Distance.swift
+//  DistanceConverter
+//
+//  Created by Piotr Piotrowski on 11/01/2017.
+//  Copyright © 2017 Piotr Piotrowski. All rights reserved.
+//
+
+import Foundation
+
+class Distance {
+    static let kmPerMile = 1.60934
+    
+    var miles:Double
+    var km:Double {
+        get {
+            return Distance.toKm(miles:miles)
+        }
+        set(newKm) {
+            miles = Distance.toMiles(km:newKm)
+        }
+    }
+    
+    init(miles:Double) {
+        self.miles = miles
+    }
+    init(km:Double) {
+        self.miles = Distance.toMiles(km:km)
+    }
+    
+    static func toKm(miles:Double)->Double {
+        return miles * kmPerMile
+    }
+    
+    static func toMiles(km:Double)->Double {
+        return km / kmPerMile
+    }
+}
