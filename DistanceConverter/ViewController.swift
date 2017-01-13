de6767125b22aed3e4244a6c626042a67a8e1e33
Piotr Piotrowski <piotrpiotrowski@Smarts-Mac-mini.local>
2017-01-13 13:38:51 +0100

    add buttons for converting

--- DistanceConverter/ViewController.swift
+++ DistanceConverter/ViewController.swift
@@ -14,18 +14,32 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var milesTextField: UITextField!
     @IBOutlet weak var kmTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         milesTextField.text = "\(distance.miles)"
         kmTextField.text = "\(distance.km)"
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func convertToKm(_ sender: AnyObject) {
+        if let miles = Double(milesTextField.text!) {
+            distance.miles = miles
+            kmTextField.text = "\(Int(distance.km))"
+        }
+    }
+    
+    @IBAction func convertToMiles(_ sender: AnyObject) {
+        if let km = Double(kmTextField.text!) {
+            distance.km = km
+            milesTextField.text = "\(Int(distance.miles))"
+        }
+    }
 
 }
 
